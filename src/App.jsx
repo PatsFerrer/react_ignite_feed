c0a5aee188d4db6b0432abfe797569d34352ada2
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-12-01 20:53:43 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Header } from './components/Header'
 import { Post } from './components/Post'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 export function App() {
   return (
     <div>
       <Header />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
