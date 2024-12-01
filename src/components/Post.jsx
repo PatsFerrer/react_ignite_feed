1d973611cf73f1863b508c6c80ceb383dc597fd9
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-12-01 20:54:30 -0300

    Estrutura da aplicação - Componente: Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,51 @@
-export const Post = () => {
-  return <div>Post</div>
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src="https://github.com/patsferrer.png" />
+          <div className={styles.authorInfo}>
+            <strong>Patricia Ferrer</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Eaí, galeros 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz
+          no NLW da Rocket. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  )
 }
