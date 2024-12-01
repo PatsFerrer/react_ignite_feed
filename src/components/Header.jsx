0ac53387d7b733a07cf172686046e0e1073eaea4
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-12-01 17:34:09 -0300

    aprendendo como utiliza CSS modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css' // CSS modules
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <strong>Ignite Feed</strong>
+    </div>
+  )
+}
