223df87111e02b476af5e9ae4af035a7d1e5308e
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-12-01 21:06:50 -0300

    finalizando componente comentarios

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,10 +1,10 @@
 import styles from './Avatar.module.css'
 
-export const Avatar = (props) => {
+export const Avatar = ({ hasBorder = true, src }) => {
   return (
     <img
-        className={styles.avatar}
-        src={props.src}
-      />
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      src={src}
+    />
   )
 }
