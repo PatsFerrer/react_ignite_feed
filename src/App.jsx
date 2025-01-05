d53d7f3ea7cf53096b42bae56391f8abe1765569
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-01-05 19:31:10 -0300

    mock de Posts + props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
-import { Header } from './components/Header'
-import { Post } from './components/Post'
-import './global.css'
-import styles from './App.module.css'
-import { Sidebar } from './components/Sidebar'
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/patsferrer.png",
+      name: "Patricia",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "E aí, galeros! 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Mais um projeto pro meu portfólio. É um projeto que fiz com a Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2025-01-04 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego",
+      role: "CEO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2025-01-03 20:00:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -10,9 +50,17 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
-  )
+  );
 }
