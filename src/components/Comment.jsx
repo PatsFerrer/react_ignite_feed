072ca5a580e43f56caa4949ed770f66ccc517c72
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-01-05 20:36:25 -0300

    props de Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,8 @@
-import { Avatar } from './Avatar'
-import styles from './Comment.module.css'
-import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
+import { ThumbsUp, Trash } from "phosphor-react";
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
@@ -19,7 +19,7 @@ export function Comment() {
               <Trash size={24} />
             </button>
           </header>
-          <p>Muito bom Pats, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
         <footer>
           <button>
@@ -29,5 +29,5 @@ export function Comment() {
         </footer>
       </div>
     </div>
-  )
+  );
 }
